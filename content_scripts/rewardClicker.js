e2071253116e228eb8d572051b835ef3e93a3bcc
jamisonl <jlevitin@gmail.com>
2021-02-06 11:50:10 -0800

    track points multiplier. Declare className for obs

--- content_scripts/rewardClicker.js
+++ content_scripts/rewardClicker.js
@@ -2,6 +2,7 @@
     If the points object doesnt exist in local storage, instantiate it.
     Else update points object, and send message to manageRewardClicker to retrieve points.
   */
+const pointsParentNode = "community-points-summary";
 const handlePointsUpdate = async (pointsNum) => {
   let title = document.URL.split("/")[document.URL.split("/").length - 1].split(
     "?"
@@ -26,15 +27,21 @@ const clicker = () => {
   let button = document.body.querySelector(".tw-button, .tw-button--success");
   if (!!button) {
     button.click();
-    handlePointsUpdate(50);
+    setTimeout(() => {
+      handlePointsUpdate(getPointsCount());
+    }, 500);
   }
 };
 
-let parentNodeClass = "community-points-summary";
-const config = { childList: true, subtree: true };
-let parentNode = document.getElementsByClassName(parentNodeClass)[0];
+const getPointsCount = () => {
+  const pointsNode = document.getElementsByClassName(pointsParentNode)[0];
+  const pointsNum = parseInt(pointsNode.textContent.split("+")[1]);
+  return !!pointsNum ? pointsNum : 50;
+};
+
+const obsConfig = { childList: true, subtree: true };
 const observer = new MutationObserver((mutationsList) => {
-  for (let mutation of mutationsList) {
+  for (const mutation of mutationsList) {
     if (
       mutation.type === "childList" &&
       mutation.target.className === "tw-full-height tw-relative tw-z-above"
@@ -44,23 +51,25 @@ const observer = new MutationObserver((mutationsList) => {
   }
 });
 
-let currentTitle = document.getElementsByTagName("title")[0];
 const titleObsConfig = { attributes: true, childList: true, subtree: true };
 const titleObserver = new MutationObserver((mutationsList) => {
   for (const mutation of mutationsList) {
     if (mutation.type === "childList") {
       //wait for DOM tree to finish..
       setTimeout(() => {
-        let parentNodeClass = "community-points-summary";
-        let parentNode = document.getElementsByClassName(parentNodeClass)[0];
+        let parentNode = document.getElementsByClassName(pointsParentNode)[0];
         clicker();
         observer.disconnect();
-        observer.observe(parentNode, config);
-      }, 3000);
+        observer.observe(parentNode, obsConfig);
+      }, 5000);
     }
   }
 });
 
-titleObserver.observe(currentTitle, titleObsConfig);
-observer.observe(parentNode, config);
+setTimeout(() => {
+  let currentTitle = document.getElementsByTagName("title")[0];
+  let parentNode = document.getElementsByClassName(pointsParentNode)[0];
+  titleObserver.observe(currentTitle, titleObsConfig);
+  observer.observe(parentNode, obsConfig);
+}, 5000);
 setTimeout(() => clicker(), 3000);
