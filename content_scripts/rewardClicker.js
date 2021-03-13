7bf51165fef9ffe141e30e010962b93f9ea13905
jamisonl <jlevitin@gmail.com>
2021-03-12 21:20:43 -0800

    more accurately tally points

--- content_scripts/rewardClicker.js
+++ content_scripts/rewardClicker.js
@@ -1,8 +1,4 @@
-/*
-    If the points object doesnt exist in local storage, instantiate it.
-    Else update points object, and send message to manageRewardClicker to retrieve points.
-  */
-const pointsParentNode = "community-points-summary";
+const pointsParentNode = ".community-points-summary";
 const handlePointsUpdate = async (pointsNum) => {
   let title = document.URL.split("/")[document.URL.split("/").length - 1].split(
     "?"
@@ -25,28 +21,47 @@ const handlePointsUpdate = async (pointsNum) => {
 
 const clicker = () => {
   let button = document.body.querySelector(".tw-button, .tw-button--success");
-  if (!!button) {
-    button.click();
+  const pointsEvent = (event) => {
     setTimeout(() => {
       handlePointsUpdate(getPointsCount());
-    }, 500);
+    }, 1000);
+  };
+  if (!!button) {
+    button.addEventListener("click", pointsEvent);
+    button.click();
+    button.removeEventListener("click", pointsEvent);
   }
 };
 
 const getPointsCount = () => {
-  const pointsNode = document.getElementsByClassName(pointsParentNode)[0];
+  const pointsNode = document.querySelector(pointsParentNode);
   const pointsNum = parseInt(pointsNode.textContent.split("+")[1]);
   return !!pointsNum ? pointsNum : 50;
+};
+
+/**
+ * @param {HTMLCollection | Element} target
+ */
+const classListSearch = (target) => {
+  if (
+    target.classList &&
+    [...target.classList].includes("tw-button--success")
+  ) {
+    return clicker();
+  }
+  if (!!target.children) {
+    for (const child of target.children) {
+      classListSearch(child);
+    }
+  }
+  return;
 };
 
 const obsConfig = { childList: true, subtree: true };
 const observer = new MutationObserver((mutationsList) => {
   for (const mutation of mutationsList) {
-    if (
-      mutation.type === "childList" &&
-      mutation.target.className === "tw-full-height tw-relative tw-z-above"
-    ) {
-      clicker();
+    if (mutation.type === "childList") {
+      classListSearch(mutation.target);
     }
   }
 });
@@ -56,20 +71,23 @@ const titleObserver = new MutationObserver((mutationsList) => {
   for (const mutation of mutationsList) {
     if (mutation.type === "childList") {
       //wait for DOM tree to finish..
+      observer.disconnect();
       setTimeout(() => {
-        let parentNode = document.getElementsByClassName(pointsParentNode)[0];
+        let parentNode = document.querySelector(pointsParentNode);
         clicker();
-        observer.disconnect();
         observer.observe(parentNode, obsConfig);
       }, 5000);
     }
   }
 });
 
-setTimeout(() => {
+try {
+  clicker();
   let currentTitle = document.getElementsByTagName("title")[0];
-  let parentNode = document.getElementsByClassName(pointsParentNode)[0];
+  let parentNode = document.querySelector(pointsParentNode);
   titleObserver.observe(currentTitle, titleObsConfig);
   observer.observe(parentNode, obsConfig);
-}, 5000);
-setTimeout(() => clicker(), 3000);
+} catch (e) {
+  {
+  }
+}
